28774f6b837286478dc8c03d62f8de59adcab3df
Orkun Gürsel <orkungrsl@gmail.com>
2022-03-25 03:14:31 +0300

    feat: update user model

--- internal/domain/model/user.go
+++ internal/domain/model/user.go
@@ -7,34 +7,35 @@ import (
 )
 
 type User struct {
-	UserID    primitive.ObjectID `json:"user_id" bson:"_id,omitempty" redis:"user_id" validate:"omitempty"`
+	Id        primitive.ObjectID `json:"id" bson:"_id,omitempty" redis:"id" validate:"omitempty"`
 	FirstName string             `json:"first_name" bson:"first_name,omitempty" redis:"first_name" validate:"required,lte=30"`
 	LastName  string             `json:"last_name" bson:"last_name,omitempty" redis:"last_name" validate:"required,lte=30"`
-	Email     string             `json:"email,omitempty" bson:"email,omitempty" redis:"email" validate:"omitempty,lte=60,email"`
-	Password  string             `json:"password,omitempty" bson:"password,omitempty" redis:"password" validate:"omitempty,required,gte=6"`
-	Role      *string            `json:"role,omitempty" bson:"role,omitempty" redis:"role" validate:"omitempty,lte=10"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty" redis:"email" validate:"omitempty,lte=100,email"`
+	Password  string             `json:"-,omitempty" bson:"password,omitempty" redis:"password" validate:"omitempty,required,gte=6,lte=60"`
+	Role      string             `json:"role,omitempty" bson:"role,omitempty" redis:"role" validate:"omitempty,lte=10"`
+	Avatar    string             `json:"avatar,omitempty" bson:"avatar,omitempty" redis:"avatar" validate:"omitempty"`
 	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty" redis:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty" redis:"updated_at"`
 } // @name User
 
-// GetUserID returns the user id
-func (u *User) GetUserID() primitive.ObjectID {
-	return u.UserID
+// GetId returns the user id
+func (u *User) GetId() primitive.ObjectID {
+	return u.Id
 }
 
-// GetUserIDString returns the user id as a string
-func (u *User) GetUserIDString() string {
-	if u.UserID.IsZero() {
+// GetIdString returns the user id as a string
+func (u *User) GetIdString() string {
+	if u.Id.IsZero() {
 		return ""
 	}
 
-	return u.GetUserID().Hex()
+	return u.GetId().Hex()
 }
 
 // GetRole returns the role of the user
 func (u *User) GetRole() string {
-	if u.Role != nil {
-		return *u.Role
+	if u.Role != "" {
+		return u.Role
 	}
 
 	return RoleUser
