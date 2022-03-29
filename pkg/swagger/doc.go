2a65583ba348d4375cdebbc8ef463b13bd6fa8c2
Orkun Gürsel <orkungrsl@gmail.com>
2022-03-30 00:39:18 +0300

    doc: remove old swagger headers

--- pkg/swagger/doc.go
+++ pkg/swagger/doc.go
@@ -1,16 +1,1 @@
 package swagger
-
-// @title     Hey Taxi Identity API
-// @version   1.0
-// @BasePath  /api/v1
-
-// @contact.name  Orkun Gürsel
-// @contact.url   https://github.com/orkungursel
-
-// @license.name  Apache 2.0
-// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
-
-// @securityDefinitions.apikey  BearerAuth
-// @in                          header
-// @name                        Authorization
-// @desc                        Add Bearer token to the request header
