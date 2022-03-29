e28fb79db861f2adf7895cd0268c758cfac920e2
Orkun Gürsel <orkungrsl@gmail.com>
2022-03-29 05:06:11 +0300

    feat: make main func compatible with server's plug api

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"context"
+	"os"
 
 	"github.com/orkungursel/hey-taxi-identity-api/config"
-	auth "github.com/orkungursel/hey-taxi-identity-api/internal/api/http"
+	_ "github.com/orkungursel/hey-taxi-identity-api/internal/api"
+	"github.com/orkungursel/hey-taxi-identity-api/internal/server"
 	"github.com/orkungursel/hey-taxi-identity-api/pkg/logger"
-	"github.com/orkungursel/hey-taxi-identity-api/pkg/mongo"
-	"github.com/orkungursel/hey-taxi-identity-api/pkg/server"
-	"github.com/orkungursel/hey-taxi-identity-api/pkg/server/swagger"
+	_ "github.com/orkungursel/hey-taxi-identity-api/pkg/swagger"
 )
 
 func main() {
@@ -16,40 +16,13 @@ func main() {
 	defer cancel()
 
 	config := config.NewConfig()
-
-	loggerConfig := &logger.Config{
-		AppName:  config.App.Name,
-		LogLevel: "debug",
-		Encoder:  "console",
-		DevMode:  true,
-	}
-	if config.IsProduction() {
-		loggerConfig.LogLevel = "info"
-		loggerConfig.Encoder = "json"
-		loggerConfig.DevMode = false
-	}
-
-	// initialize logger
-	logger := logger.New(loggerConfig)
+	logger := logger.New(config)
 	defer logger.Sync()
 
 	logger.Infof("current profile: %s", config.GetProfile())
 
-	// initilize mongo
-	mongoClient, err := mongo.Connect(ctx, config)
-	if err != nil {
-		logger.Errorf("error while connecting to mongo: %s", err)
-		return
+	if err := server.New(ctx, config, logger).Run(); err != nil {
+		logger.Errorf("error while starting server: %s", err)
+		os.Exit(1)
 	}
-	defer mongoClient.Disconnect(ctx)
-
-	s := server.New(ctx, config, logger)
-
-	// initialize auth api
-	auth.Api(config, s, mongoClient)
-
-	// initialize swagger
-	swagger.Api(s)
-
-	s.Run()
 }
