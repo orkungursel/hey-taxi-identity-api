2add327d513bdf56288afa1cca1656db2cf6b313
Orkun Gürsel <orkungrsl@gmail.com>
2022-03-29 04:52:45 +0300

    feat: implement user grpc service

--- internal/api/grpc/user_service.go
+++ internal/api/grpc/user_service.go
@@ -2,32 +2,52 @@ package grpc
 
 import (
 	"context"
+	"strings"
 
+	"github.com/orkungursel/hey-taxi-identity-api/internal/app"
 	"github.com/orkungursel/hey-taxi-identity-api/pkg/logger"
-	userService "github.com/orkungursel/hey-taxi-identity-api/proto"
+	. "github.com/orkungursel/hey-taxi-identity-api/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 )
 
-type UserServiceGrpc struct {
+type GrpcUserService struct {
 	logger logger.ILogger
-	userService.UnimplementedUserServiceServer
+	svc    app.UserService
+	tks    app.TokenService
+	UnimplementedUserServiceServer
 }
 
-func NewUserServiceGrpc(logger logger.ILogger) *UserServiceGrpc {
-	return &UserServiceGrpc{
+func NewGrpcUserService(logger logger.ILogger, svc app.UserService, tks app.TokenService) *GrpcUserService {
+	return &GrpcUserService{
 		logger: logger,
+		svc:    svc,
+		tks:    tks,
 	}
 }
 
-func (s *UserServiceGrpc) GetUserInfo(ctx context.Context, r *userService.GetUserInfoRequest) (*userService.GetUserInfoResponse, error) {
-	s.logger.Debugf("Token: %s", r.Token)
+func (s *GrpcUserService) Register(reg grpc.ServiceRegistrar) {
+	RegisterUserServiceServer(reg, s)
+}
 
-	out := &userService.GetUserInfoResponse{
-		UserId:    "123",
-		Email:     "orquncom@gmail.com",
-		FirstName: "Orkun",
-		Type:      "driver",
-		Role:      "admin",
-		Avatar:    "https://avatars0.githubusercontent.com/u/17098981?s=460&v=4",
+func (s *GrpcUserService) GetUserInfo(ctx context.Context, r *GetUserInfoRequest) (*GetUserInfoResponse, error) {
+	users, err := s.svc.UsersByIds(ctx, r.UserIds)
+	if err != nil {
+		return nil, grpc.Errorf(codes.Internal, "failed to get users: %s", err)
 	}
-	return out, nil
+
+	usersData := make([]*UserInfo, 0)
+	for _, user := range users {
+		usersData = append(usersData, &UserInfo{
+			Id:     user.Id,
+			Email:  user.Email,
+			Name:   strings.Join([]string{user.FirstName, user.LastName}, " "),
+			Role:   user.Role,
+			Avatar: user.Avatar,
+		})
+	}
+
+	return &GetUserInfoResponse{
+		Users: usersData,
+	}, nil
 }
