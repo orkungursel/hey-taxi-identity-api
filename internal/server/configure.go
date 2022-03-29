0036206db6cb15b2fc04f2ab92fabc697e21ac31
Orkun Gürsel <orkungrsl@gmail.com>
2022-03-29 04:39:40 +0300

    feat: service package remastered
    Http and GRPC layers have been separated.

---
+++ internal/server/configure.go
@@ -0,0 +1,23 @@
+package server
+
+import (
+	"time"
+
+	emw "github.com/labstack/echo/v4/middleware"
+	"github.com/orkungursel/hey-taxi-identity-api/internal/server/middleware"
+)
+
+// configure the echo server
+func (s *Server) configure() {
+	s.echo.HidePort = true
+	s.echo.HideBanner = true
+
+	// add pre middlewares
+	s.echo.Pre(middleware.AddTrailingSlash())
+	s.echo.Pre(middleware.Logger(s.logger))
+
+	// add middlewares
+	s.echo.Use(emw.Recover())
+	s.echo.Use(middleware.CORS(s.config))
+	s.echo.Use(emw.Secure())
+}
